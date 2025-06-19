757a385afc786851ddc00818fb7ec5eec25cf0d8
iamspur <iamspur25@gmail.com>
2025-06-19 14:29:54 +0700

    Add files via upload

---
+++ 28_input_name_age.py
@@ -0,0 +1,5 @@
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {fname} {lname}")
+print(f"You're {age} years old")
